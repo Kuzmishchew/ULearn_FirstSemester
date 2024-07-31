f71484ebdc7506fda36259da35bb1601fb90e0df
Kashmir <kuzmishchew@gmail.com>
2024-07-31 22:15:53 +0500

    INIT: repo start and done Expr1

---
+++ Первое знакомство с C#/Expr1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Как поменять местами значения двух переменных? Можно ли это сделать без ещё одной временной переменной. Стоит ли так делать?
+
+int a = 2;
+int b = 5;
+
+//Можно реализовать через присваивание кортежей https://learn.microsoft.com/ru-ru/dotnet/csharp/language-reference/builtin-types/value-tuples#tuple-assignment-and-deconstruction
+//Пока не могу ответить стоит ли так делать. Так как непонятны подводные камни этого инструмента.
+(a, b) = (b, a);
+
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
