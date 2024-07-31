7b745052a1a9985b9b912f286aef896a224d02b8
Kashmir <kuzmishchew@gmail.com>
2024-07-31 22:29:43 +0500

    FEAT: done Expr2

---
+++ Первое знакомство с C#/Expr2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задается натуральное трехзначное число (гарантируется, что трехзначное). Развернуть его, т.е. получить трехзначное число, записанное теми же цифрами в обратном порядке.
+namespace Expr2
+{
+    internal class Program
+    {
+        static int ReverseNumber(int number)
+        {
+            string numberString = number.ToString();
+
+            string result = numberString[2].ToString() + numberString[1].ToString() + numberString[0].ToString();
+
+            return Convert.ToInt32(result);
+        }
+        static void Main(string[] args)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(ReverseNumber(number));
+        }
+    }
+}
